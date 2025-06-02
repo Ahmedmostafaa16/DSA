cd64387467939d7c95d9b87f7948d450b344cdcc
Ahmedmostafaa16 <ahmedmostafaa889@gmail.com>
2025-06-02 17:45:18 +0300

    Update BST.py

--- BST.py
+++ BST.py
@@ -1,3 +1,5 @@
+import queue
+
 class Node :
     def __init__(self,data,left=None,right=None):
         
@@ -113,6 +115,23 @@ class TreeNode :
             self.inorder(node.left)
             print(node.data, end=' ')
             self.inorder(node.right)
+
+    
+     def bfs(self) :
+        if self.root :
+            visited_list = []
+            bfs_queue = queue.SimpleQueue()
+            bfs_queue.put(self.root)
+            
+            while not bfs_queue.empty() :
+                current_node=bfs_queue.get()
+                visited_list.append(current_node.data)
+                if current_node.left :
+                    bfs_queue.put(current_node.left)
+                    
+                if current_node.right :
+                    bfs_queue.put(current_node.right)    
+        return visited_list        
              
              
 
